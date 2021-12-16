4837b6a0498e6a07b1a5583cdfd2325f4a9471c8
Sasha Orlov <95905112+SashaOrlov2004@users.noreply.github.com>
2021-12-16 10:33:28 +0300

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
+#include <Windows.h>
+#include <string>
 using namespace::std;
 
 
 string Copy(string S, int n1, int n2) {
 	string str = "";
-	for (int i = n1; i < (n2 + 1); i++){
+	for (int i = n1; i < (n2 +1); i++){
 		str += S[i];
 	}
 	return str;
 }
-
 int main() {
-	setlocale(0, "");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
 	string S;
 	cout << "Введите строку" << endl;
-	cin >> S;
+	getline(cin, S);
+
 
 	int n1;
 	cout << "Введите первую границу строки" << endl;
 	cin >> n1;
+
 
 	int n2;
 	cout << "Введите вторую границу строки" << endl;
